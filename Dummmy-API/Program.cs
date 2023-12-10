b90363a658ffbeb18fac3f968df726cfa697fbfe
mbesher2011 <mbesher2011@gmail.com>
2023-12-10 10:21:39 +0300

    haproxy load balancer with three containers

---
+++ Dummmy-API/Program.cs
@@ -0,0 +1,12 @@
+﻿var builder = WebApplication.CreateBuilder();
+var app = builder.Build();
+
+app.MapGet("", (HttpContext httpContext) => {
+    return $"the running app has {httpContext.Connection.LocalPort}";
+});
+
+
+app.MapGet("/test",() => {
+    return $"working fine.";
+});
+app.Run();
